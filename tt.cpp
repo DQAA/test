f67e444a737eefc024e41532639b624ceab6f00a
DQAA <55402342+DQAA@users.noreply.github.com>
2020-04-13 11:00:39 +0800

    Update tt.cpp

--- tt.cpp
+++ tt.cpp
@@ -1,16 +1,22 @@
 #include <bits/stdc++.h>
 #define N 600005
 #define M 300005
-#define num_of_pth 4
 
 using namespace std;
 
-struct timeval Begin;
+struct timeval start;
 struct timeval End;
 double timer;
 
-vector<int> g[N];
-vector<int> fg[N];
+struct node{
+    int next;
+    int id;
+}g[N];
+struct nde{
+    int to;
+    int next;
+}edge[M];
+int tot;
 
 struct nd{
     int from;
@@ -18,17 +24,54 @@ struct nd{
 }rd[M];
 map<int,int> mp;
 map<int,bool> is;
-int yid[N];
-int n,m;
+
+int cmp(node a,node b)
+{
+    if(a.id < b.id)return 1;
+    else return 0;
+}
 
 void addEdge(int s,int t)
 {
-	g[s].push_back(t);
+    edge[++tot].to = t;
+    edge[tot].next = g[s].next;
+    g[s].next = tot;
 }
 
-int isD[num_of_pth][N];
-int stk[num_of_pth][10];
-int top[num_of_pth];
+int isD0[N],isD1[N];
+int stk0[10],stk1[10];
+int top0,top1;
+int n,m;
+/*struct A{
+    int id;
+    int num;
+    A* next;
+    bool operator == (const struct A b) const{
+        if(this->num != b.num || this->id != b.id)return 0;
+        const A* p = this;
+        const A* q = &b;
+        while(p != NULL && q != NULL)
+        {
+            if(p->id != q->id)return 0;
+            p = p->next;q = q->next;
+        }
+        return 1;
+    }
+    bool operator < (const struct A b) const{
+        if(this->num < b.num)return 1;
+        else if(this->num == b.num){
+            const A* p = this;
+            const A* q = &b;
+            for(int i = 0;i < num;i++)
+            {
+                if(p->id < q->id)return  1;
+                else if(p->id > q->id)return 0;
+                p = p->next;q = q->next;
+            }
+        }
+        return 0;
+    }
+}ans[3000005];*/
 struct A{
 	int id[8];//7时排序出错，过程未知
 	int num;
@@ -46,156 +89,161 @@ struct A{
 		}
 		return 0;
 	}
-}ans[num_of_pth][3000005];
-int anstot[num_of_pth];
-void creatAns(int id)
+}ans0[3000005];
+A ans1[3000005];
+int anstot0,anstot1,anstot2,anstot3;
+/*void creatAns()
 {
-	ans[id][anstot[id]].id[0] = yid[stk[id][0]];
-	ans[id][anstot[id]].num = top[id];
-	for(int i = 1;i < top[id];i++)
-		ans[id][anstot[id]].id[i] = yid[stk[id][i]];
-	ans[id][anstot[id]++].id[top[id]] = -1;
-}
-int Next[num_of_pth];
-int in[N];
-int out[N];
-void DFS(int first,int id,int pid)
-{
-	if(top[pid] >= 7)return;
-    if(id < first)return;
-	if(out[id] == -1)return;
-    isD[pid][id] = 1;
-    stk[pid][top[pid]++] = id;
-	vector<int>:: iterator j = g[id].begin();
-    while(j != g[id].end())
+    A* p = &ans[anstot++];
+    p->id = g[stk[0]].id;
+    p->num = top;
+    for(int i = 1;i < top;i++)
     {
-        Next[pid] = *j;
-        if(isD[pid][Next] == 1){
-            if(top[pid] >= 3 && Next[pid] == stk[pid][0])creatAns(pid);
-        }else{
-            DFS(first,Next[pid],pid);
-        }
-        j++;
+        p->next = new A;
+        p = p->next;
+        p->id = g[stk[i]].id;
+        p->next = NULL;
     }
-    isD[pid][id] = 0;
-    top[pid]--;
+}*/
+void creatAns0()
+{
+	ans0[anstot0].id[0] = g[stk0[0]].id;
+	ans0[anstot0].num = top0;
+	for(int i = 1;i < top0;i++)
+		ans0[anstot0].id[i] = g[stk0[i]].id;
+	ans0[anstot0++].id[top0] = -1;
+}
+int Next0,Next1;
+void DFS0(int first,int id)
+{
+	if(top0 >= 7)return;
+    if(id < first)return;
+    isD0[id] = 1;
+    stk0[top0++] = id;
+    int j = g[id].next;
+    while(j)
+    {
+        Next0 = edge[j].to;
+        if(isD0[Next0] == 1){
+            if(top0 >= 3 && Next0 == stk0[0])creatAns0();
+        }else{
+            DFS0(first,Next0);
+        }
+        j = edge[j].next;
+    }
+    isD0[id] = 0;
+    top0--;
     return;
 }
-int Min(int a,int b)
+void creatAns1()
 {
-	if(a < b)return a;
-	else return b;
+	ans1[anstot1].id[0] = g[stk1[0]].id;
+	ans1[anstot1].num = top1;
+	for(int i = 1;i < top1;i++)
+		ans1[anstot1].id[i] = g[stk1[i]].id;
+	ans1[anstot1++].id[top1] = -1;
 }
-void *th(void *ID)
+void DFS1(int first,int id)
 {
-	long long id = (long long)ID;
-	for(int i = id*(n/num_of_pth) + 1;i < Min((id+1)*(n/num_of_pth),n) + 1;i++)
-	{
-		DFS(i,i,id);
-	}
-}	
-void processIn(int i)
-{
-    while(g[i].begin() != g[i].end())
+	if(top1 >= 7)return;
+    if(id < first)return;
+    isD1[id] = 1;
+    stk1[top1++] = id;
+    int j = g[id].next;
+    while(j)
     {
-        if(--in[g[i][0]] == 0){
-            in[g[i][0]] = -1;
-            processIn(g[i][0]);
+        Next1 = edge[j].to;
+        if(isD1[Next1] == 1){
+            if(top1 >= 3 && Next1 == stk1[0])creatAns1();
+        }else{
+            DFS1(first,Next1);
         }
-		g[i].erase(g[i].begin());
+        j = edge[j].next;
     }
+    isD1[id] = 0;
+    top1--;
+    return;
 }
-void processOut(int i)
+void *th0(void *a)
 {
-	out[i] =  -1;
-	while(fg[i].begin() != fg[i].end())
-	{
-		//g[i].erase(it - fg[i].begin() + g[i].begin());
-		if(--out[fg[i][0]] == 0){
-			processOut(fg[i][0]);
-		}
-		fg[i].erase(fg[i].begin());
-	}
+	for(int i = 1;i < n+1;i++)
+		if(i%2 == 1)DFS0(i,i);
+}
+void *th1(void *a)
+{
+	for(int i = 1;i < n+1;i++)
+		if(i%2 == 0)DFS1(i,i);
+}
+int in[N];
+void process(int i)
+{
+    //if(in[i] == -1)return;//不用
+    while(g[i].next > 0)
+    {
+        if(--in[edge[g[i].next].to] == 0){
+            in[edge[g[i].next].to] = -1;
+            process(edge[g[i].next].to);
+        }
+        g[i].next = edge[g[i].next].next;
+    }
 }
 int main()
 {
     //freopen("/data/test_data.txt","r",stdin);
     //freopen("/projects/student/result.txt","w",stdout);
+	gettimeofday(&start,NULL);
     //int t = clock();
-	gettimeofday(&Begin,NULL);
-	memset(anstot,0,sizeof(anstot));
-	memset(top,0,sizeof(top));
-	memset(Next,0,sizeof(Next));
-	for(int i = 0;i < num_of_pth;i++)
-		for(int j = 0;j < N;j++)
-			isD[i][j] = 0;
-	for(int i = 0;i < num_of_pth;i++)
-		for(int j = 0;j < 10;j++)
-			stk[i][j] = 0;
     int tmp;
     while(scanf("%d,%d,%d",&rd[m].from,&rd[m].to,&tmp) != EOF)
     {
         if(!is[rd[m].from]){
-            yid[1+n++] = rd[m].from;
+            g[1+n++].id = rd[m].from;
             is[rd[m].from] = 1;
         }
         if(!is[rd[m].to]){
-            yid[1+n++] = rd[m].to;
+            g[1+n++].id = rd[m].to;
             is[rd[m].to] =     1;
         }
         m++;
     }
-    sort(yid+1,yid+n+1);
+    sort(g+1,g+n+1,cmp);
     for(int i = 1;i < n + 1;i++)
-        mp[yid[i]] = i;
+        mp[g[i].id] = i;
     for(int i = 0;i < m;i++)
     {
-		int s = mp[rd[i].from];
-		int t = mp[rd[i].to];
-        addEdge(s,t);
-		fg[t].push_back(s);
-        in[t]++;
-		out[s]++;
+        addEdge(mp[rd[i].from],mp[rd[i].to]);
+        in[mp[rd[i].to]]++;
     }
     for(int i = 1;i < n+1;i++)
     {
         if(in[i] == -1)continue;
-        else if(in[i] == 0)processIn(i);
+        else if(in[i] == 0)process(i);
     }
-	for(int i = 1;i < n+1;i++)
-	{
-		if(out[i] == -1)continue;
-		else if(out[i] == 0)processOut(i);
-	}
-	pthread_t threads[num_of_pth];
-	for(int i = 0;i < num_of_pth;i++)
-		pthread_create(&threads[i],NULL,th,(void*)i);
-	for(int i = 0;i < num_of_pth;i++)
-		pthread_join(threads[i],NULL);
+	pthread_t threads[2];
+	pthread_create(&threads[0],NULL,th0,NULL);
+	pthread_create(&threads[1],NULL,th1,NULL);
+	pthread_join(threads[0],NULL);
+	pthread_join(threads[1],NULL);
+	int anstot = anstot0 + anstot1;
+	for(int i = 0;i < anstot1;i++)
+		ans0[i + anstot0] = ans1[i];
+	sort(ans0,ans0+anstot);
     /*for(int i = 1;i < n+1;i++)
-        if(isD[i] == 0 && in[i] != 0)DFS(i,i);*/
-	for(int i = 1;i < num_of_pth;i++)
-	{
-		for(int j = 0;j < anstot[i];i++)
-		{
-			ans[0][anstot[0]+j] = ans[i][j];
-		}
-		anstot[0] += anstot[i];
-	}
-    sort(&ans[0],(&ans[0])+anstot[0]);
-    printf("%d\n",anstot[0]);
+        if(isD[i] == 0 && in[i] != 0)DFS(i,i);
+    sort(ans,ans+anstot);*/
+    printf("%d\n",anstot);
+	gettimeofday(&End,NULL);
+	timer = 1000000 * (End.tv_sec - start.tv_sec) + End.tv_usec - start.tv_usec;
+	printf("%fs\n",timer/1000000);
 	//double ti = clock() - t;
     //printf("%fs\n",ti/CLOCKS_PER_SEC);
-	gettimeofday(&End,NULL);
-	timer = 1000000 * (End.tv_sec - Begin.tv_sec) + (End.tv_usec - Begin.tv_usec);
-	printf("%fs\n",timer/1000000);
     /*for(int i = 0;i < anstot;i++)
     {
-        printf("%d",ans[i].id[0]);
+        printf("%d",ans0[i].id[0]);
         int p = 1;
-        while(ans[i].id[p] != -1)
+        while(ans0[i].id[p] != -1)
         {
-            printf(",%d",ans[i].id[p]);
+            printf(",%d",ans0[i].id[p]);
             p++;
         }
         printf("\n");
